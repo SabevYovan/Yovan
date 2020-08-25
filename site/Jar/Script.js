2d19afb102f40ba109acaaaa91ec5ac1ef5d3005
Yovan Sabev <y.sabev@dssolutions.bg>
2020-08-25 13:48:52 +0300

    azdas

--- site/Jar/Script.js
+++ site/Jar/Script.js
@@ -1,11 +1,24 @@
 function sayHello () {
-    var name = document.getElementById("name").Value;
-    var message = "<h2>Hello " + name + "!</h2>";
-
-    document.getElementById("content").innerHTML = message;
+    var name =
+     document.getElementById("name").value;
+     var message = "<h2>Hello " + name + "!</h2>";
+  
+    // document
+    //   .getElementById("content")
+    //   .textContent = message;
+  
+    document
+      .getElementById("content")
+      .innerHTML = message;
+  
     if (name === "student") {
-        var title = document.querySelector("#title").textContent;
-        title += " & Lovin' it!";
-        document.querySelector("#title").textContent = title;
+      var title = 
+        document
+          .querySelector("#title")
+          .textContent;
+      title += " & Lovin' it!";
+      document
+          .querySelector("h1")
+          .textContent = title;
     }
-}
+  }
