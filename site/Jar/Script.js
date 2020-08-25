c40c596f09bf68c6b0db63a97489f2b4c9883b44
Yovan Sabev <y.sabev@dssolutions.bg>
2020-08-25 13:43:54 +0300

    ada

--- site/Jar/Script.js
+++ site/Jar/Script.js
@@ -1,1 +1,11 @@
-var x = "Hello World"
+function sayHello () {
+    var name = document.getElementById("name").nodeValue;
+    var message = "<h2>Hello " + name + "!</h2>";
+
+    document.getElementById("content").innerHTML = message;
+    if (name === "student") {
+        var title = document.querySelector("#title").textContent;
+        title += " & Lovin' it!";
+        document.querySelector("#title").textContent = title;
+    }
+}
